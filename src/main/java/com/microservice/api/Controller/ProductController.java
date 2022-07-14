01c5347362d93a60dccc56a38e6848f846877e88
NadaBENTAARIT <nada123.bentaarit@gmail.com>
2022-07-14 13:31:47 +0200

    test

--- src/main/java/com/microservice/api/Controller/ProductController.java
+++ src/main/java/com/microservice/api/Controller/ProductController.java
@@ -1,10 +1,10 @@
 package com.microservice.api.Controller;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,44 +21,49 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProductController {
-  @Autowired
-  private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-  HttpHeaders createHeaders() {
-    return new HttpHeaders() {
-      {
-        String authHeader = "Bearer YjQwYWVhNTg2MWRhZmUwYjk4YWJlNzY5Y2Q1YjlkYjE5NzY1YTUwMzM2ZTM5NDM1Yjc3M2MzYmExNTI1OWE2Zg";
-        set("Authorization", authHeader);
-      }
-    };
-  }
+    HttpHeaders createHeaders() {
+        return new HttpHeaders() {
+            {
+                String authHeader = "Bearer YjQwYWVhNTg2MWRhZmUwYjk4YWJlNzY5Y2Q1YjlkYjE5NzY1YTUwMzM2ZTM5NDM1Yjc3M2MzYmExNTI1OWE2Zg";
+                set("Authorization", authHeader);
+            }
+        };
+    }
 
-  //get product by id
+    //get product by id
   /*@RequestMapping(value = "/produit/{id}")
   public String getProductById(@PathVariable("id") long id) {
     return restTemplate.exchange("https://api.bigbuy.eu/rest/catalog/product/"+id +".json", HttpMethod.GET,new HttpEntity<String>(createHeaders()), String.class).getBody();
   }*/
 
-  //get product by id
-  @RequestMapping(value = "/produit/{id}")
-  public Object getProductById(@PathVariable("id") long id) {
-    Object product = restTemplate.exchange("https://api.bigbuy.eu/rest/catalog/product/"+id +".json", HttpMethod.GET,new HttpEntity<String>(createHeaders()), Object.class).getBody();
-    return product;
-  }
+    //get product by id
+    @RequestMapping(value = "/produit/{id}")
+    public Object getProductById(@PathVariable("id") long id) {
+        Object product = restTemplate.exchange("https://api.bigbuy.eu/rest/catalog/product/" + id + ".json", HttpMethod.GET, new HttpEntity<String>(createHeaders()), Object.class).getBody();
+        return product;
+    }
 
-  //get all products (specify page size and page)
-  // **** fields name and description
-  @GetMapping("/produits")
-  public List<Object> getProducts(@RequestParam("isoCode") String isoCode, @RequestParam("pageSize") long pageSize, @RequestParam("page") long page) throws IOException, JSONException {
-    String url ="https://api.bigbuy.eu/rest/catalog/productsinformation.json?isoCode="+isoCode+"&pageSize="+pageSize+"&page="+page;
-    Object[] produits = restTemplate.exchange(url, HttpMethod.GET,new HttpEntity<String>(createHeaders()), Object[].class).getBody();
-    Gson gson = new Gson();
-    //JSONObject myObject = new JSONObject(Arrays.toString(produits));
+    //get all products (specify page size and page)
+    // **** fields name and description
+    @GetMapping("/produits")
+    public List<Object> getProducts(@RequestParam("isoCode") String isoCode, @RequestParam("pageSize") long pageSize, @RequestParam("page") long page) throws IOException, JSONException {
+        String url = "https://api.bigbuy.eu/rest/catalog/productsinformation.json?isoCode=" + isoCode + "&pageSize=" + pageSize + "&page=" + page;
+        Object[] produits = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<String>(createHeaders()), Object[].class).getBody();
+        System.out.println("hello");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
 
-    gson.toJson( produits, new FileWriter("test.json")); // store result in json
+            // Writing to a file
+            mapper.writeValue(new File("test.json"), produits);
 
-System.out.println("hello");
-    return  Arrays.asList(produits);
+           } catch (IOException e) {
+            e.printStackTrace();
+                                   }
+        return  Arrays.asList(produits);
 
-  }
+
+    }
 }
